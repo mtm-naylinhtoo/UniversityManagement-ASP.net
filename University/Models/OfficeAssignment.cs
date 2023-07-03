72e3e26fe627d1cb4033c7bfbae6f15cdf1c6d78
naylin2 <nay.linhtoo@seattleconsultingmyanmar.com>
2023-07-03 20:17:01 +0630

    [add] complex data model

---
+++ University/Models/OfficeAssignment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace University.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        [ForeignKey("Instructor")]
+        public int InstructorID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public virtual Instructor Instructor { get; set; }
+    }
+}
